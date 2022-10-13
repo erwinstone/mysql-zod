cc00f0bc68f7682fe2231460e0bf2c6f89e13b2e
Erwin Stone <erwinstone@pm.me>
2022-10-13 16:07:00 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# mysql-zod
+
+Generate Zod interfaces from MySQL database
+
+## Installation
+
+Install `mysql-zod` with npm
+
+```bash
+npm install mysql-zod --save-dev
+```
+
+## Usage/Examples
+
+Create a file named `mysql-zod.json` and fill it as follows (adjust to your needs):
+
+```json
+{
+  "host": "127.0.0.1",
+  "port": 3306,
+  "user": "root",
+  "password": "secret",
+  "database": "myapp"
+}
+```
+
+Create User table:
+
+```sql
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profilePicture` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+```
+Then run the command:
+
+```bash
+npx mysql-zod
+```
+
+The above command will create a `User.ts` file with the following contents:
+
+```typescript
+import z from 'zod'
+
+export const User = z.object({
+  id: z.number(),
+  name: z.string(),
+  username: z.string(),
+  password: z.string(),
+  profilePicture: z.string().nullable().optional(),
+})
+```
+## Config
+
+`mysql-zod.json`
+```json
+{
+  "host": "127.0.0.1",
+  "port": 3306,
+  "user": "root",
+  "password": "secret",
+  "database": "myapp",
+  "tables": ["User", "Log"],
+  "ignore": ["Log"],
+  "folder": "@zod",
+  "suffix": "table"
+}
+```
+
+| Option | Description |
+| ------ | ----------- |
+| tables | Filter the tables to include only those specified. |
+| ignore | Filter the tables to exclude those specified. |
+| folder | Specify the output directory. |
+| suffix | Suffix to the name of a generated file. (eg: `User.table.ts`) |
---
+++ generator.ts
@@ -0,0 +1,112 @@
+#!/usr/bin/env node
+
+/* eslint-disable no-case-declarations */
+import path from 'path'
+import fs from 'fs-extra'
+import knex from 'knex'
+
+// Cross platform clear console.
+process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H')
+
+function getType(descType: Desc['Type'], descNull: Desc['Null']) {
+  const type = descType.split('(')[0]
+  const isNull = descNull === 'YES'
+  switch (type) {
+    case 'date':
+    case 'datetime':
+    case 'timestamp':
+    case 'time':
+    case 'year':
+    case 'char':
+    case 'varchar':
+    case 'tinytext':
+    case 'text':
+    case 'mediumtext':
+    case 'longtext':
+    case 'json':
+    case 'decimal':
+      return isNull ? 'z.string().nullable().optional()' : 'z.string()'
+    case 'tinyint':
+    case 'smallint':
+    case 'mediumint':
+    case 'int':
+    case 'bigint':
+    case 'float':
+    case 'double':
+      return isNull ? 'z.number().nullable().optional()' : 'z.number()'
+    case 'enum':
+      const value = descType.replace('enum(', '').replace(')', '').replaceAll(',', ', ')
+      return `z.enum([${value}])`
+  }
+}
+
+const config = fs.readJSONSync(path.join(process.cwd(), 'mysql-zod.json')) as Config
+if (config.folder && config.folder !== '')
+  fs.emptyDirSync(config.folder)
+
+async function generate(config: Config) {
+  const db = knex({
+    client: 'mysql2',
+    connection: {
+      host: config.host,
+      port: config.port,
+      user: config.user,
+      password: config.password,
+      database: config.database,
+    },
+  })
+
+  const t = await db.raw('SELECT table_name FROM information_schema.tables WHERE table_schema = ?', [config.database])
+  let tables = t[0].map((row: any) => row.table_name).filter((table: string) => !table.startsWith('knex_')).sort() as Tables
+
+  if (config.tables && config.tables.length)
+    tables = tables.filter(table => config.tables!.includes(table))
+
+  if (config.ignore && config.ignore.length)
+    tables = tables.filter(table => !config.ignore!.includes(table))
+
+  for (const table of tables) {
+    const d = await db.raw(`DESC ${table}`)
+    const describes = d[0] as Desc[]
+    let content = `import z from 'zod'
+
+export const ${table} = z.object({`
+    for (const desc of describes) {
+      const field = desc.Field
+      const type = getType(desc.Type, desc.Null)
+      content = `${content}
+  ${field}: ${type},`
+    }
+    content = `${content}
+})
+`
+    const dir = config.folder && config.folder !== '' ? config.folder : '.'
+    const file = config.suffix && config.suffix !== '' ? `${table}.${config.suffix}.ts` : `${table}.ts`
+    const dest = path.join(dir, file)
+    console.log('Created:', dest)
+    fs.outputFileSync(dest, content)
+  }
+  await db.destroy()
+}
+
+(async () => {
+  await generate(config)
+})()
+
+type Tables = string[]
+interface Desc {
+  Field: string
+  Type: string
+  Null: 'YES' | 'NO'
+}
+interface Config {
+  host: string
+  port: number
+  user: string
+  password: string
+  database: string
+  tables?: string[]
+  ignore?: string[]
+  folder?: string
+  suffix?: string
+}
